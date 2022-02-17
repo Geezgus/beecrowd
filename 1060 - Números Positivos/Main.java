aaaf393240449d5eeccdd752dfccf7a76185bdb1
Geezgus <gutorap.gh@gmail.com>
2022-02-16 21:53:08 -0300

    new file:   "1060 - N\303\272meros Positivos/Main.java"

---
+++ 1060 - Números Positivos/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+        final int NUMBER_OF_QUESTIONS = 6;
+
+        double[] answers = new double[NUMBER_OF_QUESTIONS];
+        int positivesCount = 0;
+
+        for (int i = 0; i < NUMBER_OF_QUESTIONS; i++) {
+            do {
+                answers[i] = input.nextDouble();
+            } while (answers[i] == 0);
+
+            if (answers[i] > 0)
+                positivesCount ++;
+        }
+
+        System.out.println(positivesCount+" valores positivos"); 
+    }
+}
